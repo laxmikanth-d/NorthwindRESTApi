958ac80b0b53b34dd92244ad5cf2665f73fea3f6
Laxmikanth Dhage <laxmikanth.dhage@gmail.com>
2022-01-28 22:54:38 -0800

    Removed mod_wsgi and apachi server related files

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from db import db
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/northwind'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lax:Par$w0RD@localhost/northwind'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lax:paS$123word@localhost/northwind'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
@@ -16,7 +16,6 @@ def home():
     return '<p>Hello World!!!</p>'
 
 api.add_resource(Order, '/order/<string:order_id>')
-# api.add_resource(Order, '/order/<int:order_id>')
 
 if __name__ == '__main__':
     db.init_app(app)
--- resources/order.py
+++ resources/order.py
@@ -3,11 +3,8 @@ from models.Orders import Orders
 
 class Order(Resource):
     
-    # def get(self, order_id):
-    #     # 10251
-    #     order = Orders.query.filter_by(order_id=order_id).first()
-    #     return order.json()
-
     def get(self, order_id):
-        order = Orders.query.filter_by(order_id=10251).first()
+        # order = Orders.query.filter_by(order_id=10251).first()
         return {'hello': order_id}
+        
+    
